3cd8343c6dc81701841d5a53f97895bf17b8082f
Erik Wang <wangyu.kami@gmail.com>
2018-05-02 18:04:52 -0400

    MongoClient accepts command from input.
    Use getattr to call method in class PlayCollections
    
    cc = create a collection
    io = insert a record into collection with 1 field
    iow = insert a record into collection with 2 fields
    sc = show collections
    dm = a dummy function

--- MongoClient.py
+++ MongoClient.py
@@ -3,42 +3,89 @@ import getpass
 
 from pymongo import MongoClient
 
-#getpass will not work in IDE environment but works in jupyter
-passwd = getpass.getpass("Enter password")
+def handle_user_command(x,dbconnection):
+    return{
+        "cc" : "createNewCollection",
+        "io" : "insertOne",
+        "iow": "insertOneWrongRecord",
+        "sc" : "showCollection",
+        "dm" : "dummy",
+    }.get(x, "Invalid command")
 
+def RunMe():
+    try:
+        dbconnection = getDBConnection()
 
-try:
-    client  = MongoClient('mongodb://erikwang:{0}@35.196.213.79:27017/cards'.format(passwd))
-    dbclient = client.cards
-    Collections = dbclient.collection_names()
-except:
-    print("!!!!!!!!!!!There is a problem when connect to mongodb, please check your password!!!!!!!!!!!!")
-    raise SystemExit
-
-#print(type(Collections))
-
-
-#To get a single colleciton from databse cards
-#Cards_collection = dbclient.MyHsCards
-
-if len(Collections) > 1:
-    #Get a confirmation (y) from console
-    flag = input("Found more than one Collections in the database, input [y] if you want to show them all, otherwise I will exit....")
-    if  flag != 'y':
-        print("{0} entered, bye".format(flag))
+    except:
+        print("!!!!!!!!!!!There is a problem when connect to mongodb, please check your password!!!!!!!!!!!!")
         raise SystemExit
-elif len(Collections) < 1:
-    print("No collections in {1}".format(dbclient.full_name))
 
-#To traversal all collections in a db
-for my_collection in Collections:
-    print("Print one record in collection [{0}]".format(dbclient[my_collection].full_name))
-    print("Total records in collection <{0}>".format(dbclient[my_collection].count()))
-    pprint.pprint(dbclient[my_collection].find_one())
-    print("==================")
+    while 1:
+        user_command = input("Please enter a command (cc/io/iow/sc/dm): ")
+        execute_method = handle_user_command(user_command,dbconnection)
+        if execute_method != "Invalid command":
+            result = getattr(PlayCollection,execute_method)(dbconnection,"ekcollection")
+            print("Command run successfully, exit message is {0}".format(result))
+        else:
+            print(execute_method)
 
 
-#for post in mycollection.find():
-    #pprint.pprint(post)
+    print("Done")
 
-print("Done")
+
+def getDBConnection():
+    # getpass will not work in IDE environment but works in jupyter
+    passwd = getpass.getpass("Enter password")
+    client = MongoClient('mongodb://erikwang:{0}@35.196.213.79:27017/cards'.format(passwd))
+    dbclient = client.cards
+    return dbclient
+
+class PlayCollection(object):
+
+    def createNewCollection(MongoClient,dummyarg):
+        MongoClient.create_collection("ekcollection")
+
+    def insertOne(MongoClient, mycollection):
+        try:
+            MongoClient[mycollection].insert_one({'name':'Erik Wang'})
+        except:
+            print("There is an error when insert")
+            exit()
+
+        print("Record with 1 field inserted into {0} Successfully".format(mycollection))
+
+    def insertOneWrongRecord(MongoClient,mycollection):
+        try:
+            MongoClient[mycollection].insert_one({'name':'Takumi','age':'20'})
+        except:
+            print("There is an error when insert")
+            exit()
+
+        print("Record with 2 fields inserted into {0} Successfully".format(mycollection))
+
+    def showCollection(MongoClient,dummyarg):
+        Collections = MongoClient.collection_names()
+        if len(Collections) > 3:
+            # Get a confirmation (y) from console
+            flag = input(
+                "Found more than one Collections in the database, input [y] if you want to show them all, otherwise I will exit....")
+            if flag != 'y':
+                print("{0} entered, bye".format(flag))
+                raise SystemExit
+        elif len(Collections) < 1:
+            print("No collections in {1}".format(MongoClient.full_name))
+
+        # To traversal all collections in a db
+        for my_collection in Collections:
+            print("Print one record in collection [{0}]".format(MongoClient[my_collection].full_name))
+            print("Total records in collection <{0}>".format(MongoClient[my_collection].count()))
+            # print(dbclient[my_collection].explain())
+
+            pprint.pprint(MongoClient[my_collection].find_one())
+            print("==================")
+
+    def dummy(dummyarg0,dummyarg1):
+        return 'Dummy'
+
+if __name__ == "__main__":
+    RunMe()
